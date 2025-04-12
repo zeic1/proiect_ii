5013cb84318eac329dc6fea57e64f1d40dccca2e
vadan-gabriel-30322 <vadan.li.gabriel@student.utcluj.ro>
2025-04-12 13:10:00 +0300

    frontend razor pages

--- PharmacyApp/PharmacyApp/Program.cs
+++ PharmacyApp/PharmacyApp/Program.cs
@@ -1,29 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using PharmacyApp.Data; // Make sure this is here
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Register Razor Pages and ApplicationDbContext
 builder.Services.AddRazorPages();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-
-app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
 
 app.MapRazorPages();
-
 app.Run();
-
-
-
